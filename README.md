b77f828801e61b580ec11c270d24c5d1a260310e
anntnzrb <anntnzrb@protonmail.com>
2021-10-29 15:48:35 -0500

    ++info concepto

--- README.md
+++ README.md
@@ -13,3 +13,9 @@
 - *Stefany Natalia Farias Mera*
   - `snfarias@espol.edu.ec`
   - **GitHub**: `stefanyfariasm`
+
+## Concepto
+
+![Concepto Proyecto](./assets/concepto-proyecto.png)
+
+Link a [Miro](https://miro.com/welcomeonboard/bjJYN0hsbGM4RXE2aVpWemV0SFk5a1Nsa3ZTeWlHS1lVeVZLUlYzaTlneHhTS1NINE0xRGh0WU1vTUN1Z0VvT3wzMDc0NDU3MzYwMDIyMTAxNjk3?invite_link_id=799343242027)
---
+++ src/main/java/ec/edu/espol/edproyectopi/MainApp.java
@@ -0,0 +1,36 @@
+package ec.edu.espol.edproyectopi;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.util.Objects;
+
+import static ec.edu.espol.edproyectopi.Config.SCENE_HEIGHT;
+import static ec.edu.espol.edproyectopi.Config.SCENE_WIDTH;
+
+public class HelloApplication extends Application {
+    public static void main(final String[] argv) {
+        launch(argv);
+    }
+
+    @Override
+    public final void start(final Stage stage) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
+
+        Scene scene = new Scene(fxmlLoader.load(), SCENE_WIDTH, SCENE_HEIGHT);
+
+        stage.setResizable(false); // no resize
+
+        stage.setTitle("Sopa de Letras -- ED Proyecto P1 2021");
+        stage.getIcons()
+             .add(new Image(Objects.requireNonNull(HelloApplication
+                     .class
+                     .getResourceAsStream("icon.png"))));
+        stage.setScene(scene);
+        stage.show();
+    }
+}
