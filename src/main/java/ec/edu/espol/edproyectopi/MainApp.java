4d358a3e1d06469fcd0391146e4069e8e098764b
anntnzrb <anntnzrb@protonmail.com>
2021-11-03 01:05:05 -0500

    Avance

--- src/main/java/ec/edu/espol/edproyectopi/MainApp.java
+++ src/main/java/ec/edu/espol/edproyectopi/MainApp.java
@@ -12,14 +12,14 @@ import java.util.Objects;
 import static ec.edu.espol.edproyectopi.Config.SCENE_HEIGHT;
 import static ec.edu.espol.edproyectopi.Config.SCENE_WIDTH;
 
-public class HelloApplication extends Application {
+public class MainApp extends Application {
     public static void main(final String[] argv) {
         launch(argv);
     }
 
     @Override
     public final void start(final Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(MainApp.class.getResource("hello-view.fxml"));
 
         Scene scene = new Scene(fxmlLoader.load(), SCENE_WIDTH, SCENE_HEIGHT);
 
@@ -27,7 +27,7 @@ public class HelloApplication extends Application {
 
         stage.setTitle("Sopa de Letras -- ED Proyecto P1 2021");
         stage.getIcons()
-             .add(new Image(Objects.requireNonNull(HelloApplication
+             .add(new Image(Objects.requireNonNull(MainApp
                      .class
                      .getResourceAsStream("icon.png"))));
         stage.setScene(scene);
--- src/main/java/ec/edu/espol/edproyectopi/MainController.java
+++ src/main/java/ec/edu/espol/edproyectopi/MainController.java
@@ -3,7 +3,7 @@ package ec.edu.espol.edproyectopi;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 
-public class HelloController {
+public class MainController {
     @FXML
     private Label welcomeText;
 
